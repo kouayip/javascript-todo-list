c849fd01b0e28f94438b7c2e66f9cf9e53be8e22
kouayip <kamwayves37@gmail.com>
2020-11-04 16:01:42 +0100

    init file and dir

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new copyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets/images/*",
+          to: "assets/images",
+          flatten: true,
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/index.html"),
+      chunks: ["main"],
+    }),
+  ],
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    inline: true,
+    open: true,
+    hot: true,
+    port: 4000,
+  },
+};
